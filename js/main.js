fd8dc784add6d71620d783cfdf81d2966e18f684
Adrian Deardorff <80deardorff@live.com>
2018-06-19 23:41:08 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,111 @@
+// VARIABLE THAT CONTAINS AN ARRAY OF OBJECTS FOR EVERY ZODIAC SIGN AND ITS PROPERTY VALUE PAIRS
+let zodiacSign = [
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  },
+
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  },
+
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  },
+
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  },
+
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  },
+
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  },
+
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  },
+
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  },
+
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  },
+
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  },
+
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  },
+
+  { sign: '',
+    image: 'img/.jpeg',
+    horoscope: ''
+  }
+];
+
+// VARIABLE THAT CONTAINS AN ARRAY FOR THE MONTHS
+let dropwDownMonth = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+// FOR LOOP FOR SETTING MONTH DROPDOWN
+for (var i = 0; i < dropwDownMonth.length; i++) {
+  dropwDownMonth[i] = document.getElementById('drop-down-month');
+}
+
+// IF STATEMENT FOR SETTING DAYS IN MONTH
+if (dropwDownMonth.value == dropwDownMonth[0 || 2 || 4 || 6 || 7 || 9 || 11]) {
+
+} else if (dropwDownMonth.value == dropwDownMonth[3 || 5 || 8 || 10]) {
+
+} else if (dropwDownMonth.value == dropwDownMonth[1]) {
+
+};
+
+// VARIABLES THAT CAN MANIPULATE HTML IMAGE AND TEXT
+let sign = document.getElementById('id');
+let image = document.getElementById('image');
+let horoscope = document.getElementById('horoscope');
+let dropwDownDay = document.getElementById('drop-down-day');
+
+// VARIABLES FOR MONTH AND DATE
+let month = document.getElementById('id');
+
+
+// FOR LOOPS FOR SETTING CONDITION OF DROPDOWN MENUS WITH CORRECT DAYS and MONTHS ALOTTED TO EACH SPECIFIC MONTH THAT IS SELECTED
+
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
+
+// FUNCTION ON A CLICK OF A BUTTON THAT RUNS A FOR LOOP COMPARING THE DAY AND MONTH SELECTED TO CATEGORIES SET FOR EACH ZODIAC SIGN; CONDITION STATEMENTS NEEDED
+
+function getSign() {
+  for (var i = 0; i < .length; i++) {
+    if (dropwDownMonth.value == && dropwDownDay.value == ) {
+
+    }
+  }
+
+}
+
+
+// zodiacSign[i].sign = sign;
+// zodiacSign[i].image = image;
+// zodiacSign[i].horoscope = text;
